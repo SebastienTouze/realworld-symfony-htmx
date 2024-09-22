7958f48c6b130cb33182d8370cbed9e5a5ddd824
Sébastien Touzé <contact@sebastien-touze.fr>
2024-09-22 23:21:08 +0200

    refacto: renaming htmx controller to clearer name

--- src/Controller/htmx/ArticleFavoriteButtonController.php
+++ src/Controller/htmx/ArticleFavoriteButtonController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('htmx/articles')]
-class ArticleHtmxController extends AbstractController
+class ArticleFavoriteButtonController extends AbstractController
 {
 
     /**

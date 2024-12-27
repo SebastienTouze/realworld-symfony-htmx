dc8770c13d618740b8d4111de07c35b9f24edaef
Sébastien Touzé <contact@sebastien-touze.fr>
2024-12-28 00:16:10 +0100

    fix redirection when interacting with HTMX elements

---
+++ src/EventSubscriber/HTMXRedirectResponseSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * This Subscriber checks if the response is a 302 with a HTMX request.
+ * If yes changes the response to a working redirection for HTMX
+ */
+class HTMXRedirectResponseSubscriber implements EventSubscriberInterface
+{
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        $response = $event->getResponse();
+        if ($response->getStatusCode() !== 302) {
+            return;
+        }
+
+        $isHTMXRequest = $event->getRequest()->headers->get('HX-Request', false);
+        if($isHTMXRequest) {
+            $redirectLocation = $response->headers->get('location');
+            $event->setResponse(new Response(null, Response::HTTP_NO_CONTENT, ['HX-Redirect' => $redirectLocation]));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+}

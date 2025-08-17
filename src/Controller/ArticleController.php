b7ea6d895618b23d7b8acb83e4076f53781a6d1b
Sébastien Touzé <contact@sebastien-touze.fr>
2025-08-17 16:38:55 +0200

    add article edition and deletion

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use App\Service\ArticleService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,24 +28,55 @@ class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route('/editor', name: 'app_editor', methods: ['GET', 'POST'])]
-    public function editor(Request $request, ArticleService $articleService): Response
+    #[Route('/editor', name: 'app_editor_new', methods: ['GET', 'POST'])]
+    public function editorNew(Request $request, ArticleService $articleService): Response
     {
-        $article = new Article();
+        return $this->manageFormAndCreateResponse(new Article(), $request, $articleService, 'app_editor_new');
+    }
+
+    /**
+     * @param Article $article
+     * @param Request $request
+     * @param ArticleService $articleService
+     * @return Response
+     */
+    public function manageFormAndCreateResponse(Article $article, Request $request, ArticleService $articleService, string $hxTarget): Response
+    {
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
                 $articleService->createOrUpdateArticle($article);
+                return $this->redirectToRoute('app_article', [
+                    'id' => $article->getId(),
+                    'slug' => $article->getSlug(),
+                ]);
             }
         }
 
         $isHTMXRequest = $request->headers->get('HX-Request', false);
         if ($isHTMXRequest) {
-            return $this->render('article/components/editor-form-partial.html.twig', ['form' => $form]);
+            return $this->render('article/components/editor-form-partial.html.twig', ['form' => $form, 'hxTarget' => $hxTarget]);
         }
 
-        return $this->render('article/editor.html.twig', ['form' => $form]);
+        return $this->render('article/editor.html.twig', ['form' => $form, 'hxTarget' => $hxTarget]);
     }
+
+    #[Route('/editor/{id:article}-{slug}', name: 'app_editor_edit', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-_]+'], methods: ['POST', 'GET'])]
+    public function editArticle(Article $article, Request $request, ArticleService $articleService): Response
+    {
+        return $this->manageFormAndCreateResponse($article, $request, $articleService, 'app_editor_edit');
+    }
+
+    // GET is allowed to keep things working without JS (so no HX-DELETE)
+    #[Route('/article/{id}/delete', name: 'app_article_delete', methods: ['GET', 'DELETE'])]
+    public function delete(Article $article, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+
 }
